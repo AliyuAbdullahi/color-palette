f76d2c4081a3f3c5f51932669983a1f8d61afbee
Aliyu Abdullahi <aliyu.abdullahiq@gmail.com>
2015-03-07 10:45:50 -0800

    Color palette first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+(function(){
+
+	var app = angular.module('myApp', []);
+	app.controller('buttonCtrl',function()
+	{
+this.product=gem;
+	});
+
+
+})();
+var gem = {
+name: 'Dodecahedron',
+price: 2.95,
+
+};
