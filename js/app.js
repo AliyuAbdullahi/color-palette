c805ccfadec5a38e83492d13ae9c9854773d9fd9
Aliyu Abdullahi <aliyu.abdullahiq@gmail.com>
2015-03-10 00:56:26 -0700

    Improve UI

---
+++ js/app.js
@@ -0,0 +1,58 @@
+var colourPallete = angular.module("colourPallete", []);
+colourPallete.controller("ColourController",["$scope",function ($scope){
+
+	$scope.colour = {
+		// red: 0,
+		// green: 0,
+		// blue: 0
+	};
+    $scope.colour2={
+        
+    };
+    
+var MyStyle = function(red,green,blue){
+this.style = {background: 'hsl('+red+','+green+'%'+','+blue+'%'+')'};
+    
+    };
+    
+    $scope.Style = [];
+   
+  
+	$scope.addColour = function (colour) {
+
+        var style = new MyStyle(colour.red, colour.green, colour.blue);
+        console.log(style);
+        $scope.Style.push(style);
+    };
+    
+    $scope.removeColor = function(index){
+    $scope.Style.splice(index,1);
+    };
+    $scope.clearAll = function(){
+    $scope.Style =[];
+        $scope.colour.red ="";
+        $scope.colour.blue="";
+        $scope.colour.green="";
+    };
+    
+    $scope.edit = function(){
+        
+    }
+    $scope.editPointColour = function(index){
+        
+    $scope.Style[index].style = {background: 'hsl('+colour2.red+','+colour2.green+'%'+','+colour2.blue+'%'+')'};
+        
+    }
+}]);
+    
+  /*  colourPallete.controller("EditController",["$scope",function($scope){
+    
+    $scope.colour2 = {
+    
+    };
+        
+        $scope.paint = function(){
+        
+        $scope.newStyle={background: 'hsl'+red+','+green+'%'+','+blue+'%'+')'};
+        };
+    }]);*/
